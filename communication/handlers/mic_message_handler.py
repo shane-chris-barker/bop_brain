211582d916cdd42db35854ba4911582ec895e254
= <geekychav@gmail.com>
2025-06-21 22:42:13 +0100

    refs feature/event-mapper - map some voice input to events

--- communication/handlers/mic_message_handler.py
+++ communication/handlers/mic_message_handler.py
@@ -1,10 +1,28 @@
 import logging
 logger = logging.getLogger(__name__)
+from bop_common.dtos.event_dto import EventDTO
 from bop_common.dtos.communication_dto import CommunicationDTO
+from bop_common.enums.event_type import EventType
 from communication.handlers.incoming_communication_message_handler_interface import \
     IncomingCommunicationMessageHandlerInterface
 
-
 class MicMessageHandler(IncomingCommunicationMessageHandlerInterface):
+    def __init__(self) -> None:
+        self.keyword_event_map = {
+            "dance": EventType.BOP_DANCE,
+            "happy": EventType.BOP_HAPPY,
+            "sad": EventType.BOP_SAD,
+            "angry": EventType.BOP_ANGRY,
+            "test": EventType.BOP_HAPPY,
+        }
+        self.log_prefix = "[🎤 MIC HANDLER]"
+
     def handle(self, dto: CommunicationDTO):
-        logger.info('Mic handler just got the message')
+        text = dto.content.get("text", "").lower()
+        logger.info(f"{self.log_prefix} is checking for the word(s): {text}")
+
+        for keyword, event_type in self.keyword_event_map.items():
+            if keyword in text:
+                event_dto = EventDTO(event_type=event_type)
+                logger.info(f"{self.log_prefix} Dispatched event:{event_type} for keyword:{keyword}")
+                break
